f26510de4c2fc620b4f28ab3cf60ccd8d45b7878
Kamogelo-Skhosana <107801605+Kamogelo-Skhosana@users.noreply.github.com>
2026-07-18 16:55:39 +0200

    Add Monaco challenge detail experience

---
+++ app/challenge/[slug]/editor.jsx
@@ -0,0 +1,7 @@
+Get-Content : A parameter cannot be found that matches parameter name 'Raw'.
+At line:2 char:47
++ Get-Content 'app/challenge/[slug]/editor.jsx' -Raw
++                                               ~~~~
+    + CategoryInfo          : InvalidArgument: (:) [Get-Content], ParameterBindingException
+    + FullyQualifiedErrorId : NamedParameterNotFound,Microsoft.PowerShell.Commands.GetContentCommand
+ 
